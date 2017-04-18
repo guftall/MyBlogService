f0c5d1b7013d9ad3ea476793d59bd416a8114d29
Omid Dehghani <guftall@chmail.ir>
2017-04-18 22:23:50 +0430

    Create Models
    اضافه کردن مدل پست و یوزر به برنامه
    که هر پست یه نویسنده داره

---
+++ MyBlogService/Models/Post.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MyBlogService.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        [Display(Name="تاریخ ارسال")]
+        public DateTime CreatedDate { get; set; }
+
+        public User Author { get; set; }
+    }
+
+    public class BlogServiceDbContext : DbContext
+    {
+
+        public DbSet<Post> Posts { get; set; }
+
+
+        public BlogServiceDbContext()
+            : base("BlogServiceDB")
+        {
+
+        }
+    }
+}
