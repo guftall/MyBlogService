17b77350018410e63c4ccff09c0cd1da49143ce3
Omid Dehghani <guftall@chmail.ir>
2017-04-18 22:06:33 +0430

    Init repo

---
+++ MyBlogService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyBlogService.MvcApplication" Language="C#" %>
---
+++ MyBlogService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBlogService.Startup))]
+namespace MyBlogService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
