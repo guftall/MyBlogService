a39ebcc7e4745c92ee9ef04e7ac08520763172fd
Omid Dehghani <guftall@chmail.ir>
2017-04-19 10:25:31 +0430

    Add BootStrap Style
    تغییر در ظاهر سایت

--- MyBlogService/App_Start/BundleConfig.cs
+++ MyBlogService/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace MyBlogService
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/blog-home.css",
+                      "~/Content/bootstrap-rtl.min.css"));
         }
     }
 }
