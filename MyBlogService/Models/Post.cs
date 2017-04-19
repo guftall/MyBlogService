d55ea2305eedcee61d293357fcbac7546e363424
Omid Dehghani <guftall@chmail.ir>
2017-04-19 10:24:34 +0430

    Add category
    اضافه کردن دسته بندی به پست ها
    تغییر نت فریم ورک به
    4.5.2

--- MyBlogService/Models/Post.cs
+++ MyBlogService/Models/Post.cs
@@ -8,13 +8,19 @@ namespace MyBlogService.Models
     public class Post
     {
         public int Id { get; set; }
+
+        [Display(Name="عنوان")]
         public string Title { get; set; }
+
+        [Display(Name="محتوا")]
         public string Content { get; set; }
 
         [Display(Name="تاریخ ارسال")]
         public DateTime CreatedDate { get; set; }
 
         public User Author { get; set; }
+
+        public Category Category { get; set; }
     }
 
     public class BlogServiceDbContext : DbContext
--- MyBlogService/Models/User.cs
+++ MyBlogService/Models/User.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyBlogService.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Display(Name="نام")]
         public string Name { get; set; }
+
+        [Display(Name = "نام خانوادگی")]
         public string Family { get; set; }
+
+        [Display(Name = "تعداد پست ارسالی")]
         public int NumberOfPosts { get; set; }
 
     }
